e4196db7634d4e18d3158577b46f9275b55c20e1
rhys5690 <rhys5690@gmail.com>
2017-06-12 13:37:09 +1000

    connected to youtube API

---
+++ src/components/video_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  return(
+    <ul className="col-md-4 list-goup">
+    <li>
+
+    </li>
+    </ul>
+  );
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAK9Bt-kwADsBF4mYulx4ctDiD2mj_x2Qg';
 
+
 //Create a new component and this component should produce some HTML
 
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] }
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
   return (
     <div>
       <SearchBar/>
+      <VideoList videos={this.state.videos}/>
     </div>
-  )
+  );
+  }
 }
 
 //Take this component's HTML and render it in the DOM (on the page)

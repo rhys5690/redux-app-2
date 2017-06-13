8dad10463dd49e70c566d7a00345ccaa3dfe66e9
rhys5690 <rhys5690@gmail.com>
2017-06-13 16:34:25 +1000

    fixed bug

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-
+const VideoListItem = (props) => {
+  const video = props.video;
+  const imageURL = video.snippet.thumbnails.default.url;
   return(
-    <li className="list-group-item "> {video}
+    <li className="list-group-item ">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object"/>
+          <img className="media-object" src={imageURL}/>
         </div>
 
         <div className="media-body">
-          <div className="media-heading"></div>
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
     </li>
